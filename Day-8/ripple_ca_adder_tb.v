0aa57bf58be4f356d5ba9eaeec80012a4cdbcfd0
dedeepyakondapalli <kondapallidedeepya0503@gmail.com>
2025-04-23 18:15:44 +0530

    Update ripple_ca_adder_tb.v

--- Day-8/ripple_ca_adder_tb.v
+++ Day-8/ripple_ca_adder_tb.v
@@ -1,30 +1,25 @@
 module ripple_ca_adder_tb();
 
-  reg [7:0] a,b;
+  reg [5:0] a,b;
   reg cin;
-  wire [7:0] sum;
+  wire [5:0] sum;
   wire carry;
 
-  int i;
+  integer i;
 
   ripple_ca_adder DUT(a,b,cin,sum,carry);
 
-  $monitor("Time = %0t, a = %b, b = %b, cin = %b, sum = %b, carry = %b",$time,a,b,cin,sum,carry);
+  initial begin
+    $monitor("time = %0t | a = %b (%0d), b = %b (%0d), cin = %b | sum = %b (%0d), carry = %b",
+             $time, a, a, b, b, cin, sum, sum, carry);
 
-  initial
-    begin
-      for(i=0;i<16;i=i+1)
-        begin
-          {a,b} = i[1:0];
-          #5;
-        end
-      repeat(8)
-        begin
-          cin = 1'b0;
-        end
-      repeat(7)
-        begin
-          cin = 1'b1;
-        end
-    end
+    a = 6'd0;   b = 6'd0;   cin = 1'b0; #5;
+    a = 6'd63;  b = 6'd63;  cin = 1'b0; #5;  // Max valid input for 6-bit
+    a = 6'd35;  b = 6'd25;  cin = 1'b1; #5;
+    a = 6'd0;   b = 6'd15;  cin = 1'b1; #5;
+    a = 6'd44;  b = 6'd5;   cin = 1'b0; #5;
+
+    $finish;
+  end
+
 endmodule
