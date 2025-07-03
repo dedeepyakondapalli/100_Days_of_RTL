026febe59319c680b2d5e11161278e4a652ed918
dedeepyakondapalli <kondapallidedeepya0503@gmail.com>
2025-07-03 19:16:35 +0530

    Create mux_and_tb.v

---
+++ Day-20/mux_and_tb.v
@@ -0,0 +1,33 @@
+module mux_and_tb;
+
+    reg a,b;
+	wire out;
+	
+	mux_and DUT(a,b,out);
+	
+	initial
+	begin
+	
+	   b = $random;
+	   a = 0;
+	   #5;
+	   $display("Time = %t | out = %b",$time,out);
+	   
+	   a = 1;
+	   #5;
+	   $display("Time = %t | out = %b",$time,out);
+	   
+	   a=0;
+	   #5;
+	   $display("Time = %t | out = %b",$time,out);
+	   
+	   a=1;
+	   #5;
+	   $display("Time = %t | out = %b",$time,out);
+	   
+	   #5;
+	   $finish;
+	   
+	end
+
+endmodule	
